5dbe552f503b709bb8437b5a795e8c82e9a60656
bendesousa <bdsousa069@gmail.com>
2022-09-02 14:42:19 +0100

    created tables in sql file

--- safari.sql
+++ safari.sql
@@ -1,3 +1,8 @@
+DROP TABLE animals;
+DROP TABLE assignments;
+DROP TABLE employees;
+DROP TABLE enclosures;
+
 CREATE TABLE enclosures(
     id SERIAL PRIMARY KEY,
     name VARCHAR (255),
@@ -5,6 +10,22 @@ CREATE TABLE enclosures(
     closedForMaintenance BOOLEAN
 );
 
-CREATE TABLE staff(
-    
+CREATE TABLE employees(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    employeeNumber INT
+);
+
+CREATE TABLE assignments(
+    id SERIAL PRIMARY KEY,
+    emmployeeId INT REFERENCES employees(id),
+    enclosureId INT REFERENCES enclosures(id),
+    day VARCHAR (255)
+);
+
+CREATE TABLE animals(
+    name VARCHAR (255),
+    type VARCHAR (255),
+    age INT,
+    enclosureId INT REFERENCES enclosures(id)
 );
